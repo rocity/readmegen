8b51258dd91f38779542649136b3dd3a203f8d16
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-09-28 13:14:48 +0800

    Send generated markdown to user

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, request
 from datetime import date
+from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create_form():
     today = date.today()
+
+    if request.method == 'POST':
+
+        file = BytesIO()
+
+        titlestr = '# %s\n' % request.form['title']
+        file.write(titlestr.encode('utf-8'))
+        file.write('## Items\n'.encode('utf-8'))
+
+        for item in request.form.getlist('item'):
+            if len(item):
+                itemstr = '- %s\n' % item
+                file.write(itemstr.encode('utf-8'))
+
+        file.seek(0)
+        return send_file(file,
+                            attachment_filename='README.md',
+                            as_attachment=True
+                            )
+
     return render_template('create.html', today=today)
